4d28ad1d7227cc058b03888f8402f33e945622da
jbonilla2 <jbonilla2@student.gsu.edu>
2018-12-04 17:13:58 -0500

    change

--- ProjectAirline/src/databaseAccess/FlightTableData.java
+++ ProjectAirline/src/databaseAccess/FlightTableData.java
@@ -27,10 +27,6 @@ public class FlightTableData {
 	                    
 	                	flightTableItems.add(new FlightTable(rs.getInt(1),rs.getString(2),rs.getString(3),
 	                    		rs.getString(4),rs.getString(5),rs.getString(6),rs.getDouble(7),rs.getInt(8)));
-<<<<<<< HEAD
-=======
-	                   
->>>>>>> branch 'master' of https://github.com/fmbarrera/ProjectAirline.git
 	                }
 	        }
 
